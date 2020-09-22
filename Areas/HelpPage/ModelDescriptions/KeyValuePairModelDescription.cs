05830f22b7f2b9e86e59ccb91787dca1b13362a7
MarAlex9 <mafernandezc@utn.edu.ec>
2020-09-22 13:00:32 -0500

    Agregar archivos de proyecto.

---
+++ Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ProyectoBodegav02.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
